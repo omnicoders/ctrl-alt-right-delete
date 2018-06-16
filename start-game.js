4f55cae83064ecf740adf334380cb65e2ec01e59
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-06-16 10:29:25 -0700

    add scenes

--- start-game.js
+++ start-game.js
@@ -6,9 +6,17 @@ const colors = require('colors');
 /*******************/
 
 let character = {
-  name: ""
+  name: "",
+  country: "",
+  stats: {
+    health: 100,
+    power: 100
+  }
 };
 
+let counter = {
+  day: 0
+};
 
 /*******************/
 /* Process         */
@@ -16,8 +24,7 @@ let character = {
 
 startGame()
 .then((result) => {
-  console.log('\nGAME OVER'.bold.white);
-  console.log('YOU FAILED\n'.bold.red);
+  gameOver(result);
 });
 
 
@@ -27,39 +34,65 @@ startGame()
 
 async function startGame(){  
   await introduction();
-  return true;
+  await sceneLoop();
+  return 'YOU ACHIEVED NOTHING';
 }
 
 async function introduction(){
-  character.name = await getCharacterName();
+  await getCharacterInfo();
   let message = `Hello ` + `${character.name}`.bold.white + `. Welcome to Ctrl-Alt-Right-Delete.\n`;
   message += 'In this game you play as a fictional member of\n';
-  message += 'the ' + 'Alt-Right'.bold.red + ' encountering various situations\n';
-  message += 'in which you must avoid being a victim of white genocide.\n';
+  message += 'the Alt-Right encountering various situations.\n';
   await messageWithPause(message);
   return true;  
 }
 
+async function sceneLoop(){
+  const scenes = require('./scenes.json');
+  await newDay();
+  let randomSceneIndex = randomNumberBetween(0, (scenes.length - 1));
+  let randomScene = scenes[randomSceneIndex];
+  let answer = await inquirer.prompt(randomScene.question);
+  console.log(`\n${answer.toQuestion}`);
 
+  return true;
+}
+
+async function newDay(){
+  counter.day++
+  console.log(`\nDay: ${counter.day}`);
+  console.log(`Health: ${character.stats.health}`);
+  console.log(`Power: ${character.stats.power}\n`);
+  return true;
+}
 
 /***********************/
 /* Character Functions */
 /***********************/
 
-async function getCharacterName() {
-  let question = [
+async function getCharacterInfo() {
+  let questions = [
     {
       type: 'input',
-      name: 'characterName',
-      message: 'Enter your Character Name:',
+      name: 'name',
+      message: 'Enter Your Character Name:',
       default: 'Milo'
-    }
+    },
+    {
+      type: 'list',
+      name: 'country',
+      message: 'What Country Is Your Character From?',
+      choices: [
+        'USA'
+      ]
+    }    
   ];
   console.log('');
-  let answer = await inquirer.prompt(question);
-  return answer.characterName;
+  let answers = await inquirer.prompt(questions);
+  character.name = answers.name;
+  character.country = answers.country;
+  return true;
 };
-
 
 
 /********************/
@@ -81,8 +114,16 @@ async function messageWithPause(message){
   if(continuePlaying){
     return true;
   } else {
-    console.log('\nGAME OVER'.bold.white);
-    console.log('YOU QUIT\n'.bold.red);
-    process.exit();
+    gameOver('YOU QUIT');
   }
+}
+
+function gameOver(failMessage){
+  console.log('\nGAME OVER'.bold.white);
+  console.log(`${failMessage}\n`.bold.red);
+  process.exit();
+}
+
+function randomNumberBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
